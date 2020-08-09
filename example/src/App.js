3d02b86355a9cabde4765e3475b1ad5446c6af28
Bruno Naschpitz <naschpitz@gmail.com>
2020-08-09 02:48:28 -0300

    .css build changed.

--- example/src/App.js
+++ example/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import _ from 'lodash';
 
 import FormInput from '@naschpitz/form-input';
+import '@naschpitz/form-input/dist/index.css';
 
 import './App.css';
 
