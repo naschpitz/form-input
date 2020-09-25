6acb3df83b31ce33c8407dfd8b138f724da05e22
Bruno Naschpitz <naschpitz@gmail.com>
2020-09-25 19:39:55 -0300

    Error correction on ButtonEnhanced import.

--- src/formInput/formInput.jsx
+++ src/formInput/formInput.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 
-import ButtonEnhanced from '@naschpitz/button-enhanced';
+import { ButtonEnhanced } from '@naschpitz/button-enhanced';
 import '@naschpitz/button-enhanced/dist/index.css';
 
 import Checkbox from "./checkbox/checkbox.jsx";
